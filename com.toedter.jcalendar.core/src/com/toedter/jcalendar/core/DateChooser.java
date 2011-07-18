307a31b32d243f79eec32466da30a64bd07dc8fa
toedter <kai@toedter.com>
2011-07-18 20:36:52 +0200

    EPL

--- com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/DateChooser.java
+++ com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/DateChooser.java
@@ -1,6 +1,15 @@
-/**
- * TODO: EPL
- */
+/*******************************************************************************
+ * Copyright (c) 2011 Kai Tödter
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kai Tödter - initial API and implementation
+ *******************************************************************************/
+
 package com.toedter.jcalendar.core;
 
 /**
