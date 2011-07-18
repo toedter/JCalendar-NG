dc08f7b252dd8314d8df20affb6b64264fa73741
toedter <kai@toedter.com>
2011-07-18 20:32:46 +0200

    initial version

---
+++ com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/DateChooser.java
@@ -0,0 +1,14 @@
+/**
+ * TODO: EPL
+ */
+package com.toedter.jcalendar.core;
+
+/**
+ * This class provides functionality for choosing a date.
+ * 
+ * @author Kai Tödter
+ *
+ */
+public class DateChooser {
+
+}
