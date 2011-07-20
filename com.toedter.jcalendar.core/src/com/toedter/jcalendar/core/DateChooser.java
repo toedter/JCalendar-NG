27cf09bf269378440a41f7de1288db46ce9a18eb
toedter <kai@toedter.com>
2011-07-20 09:38:38 +0200

    test2

--- com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/DateChooser.java
+++ com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/DateChooser.java
@@ -12,7 +12,6 @@
 
 package com.toedter.jcalendar.core;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -84,13 +83,6 @@ public class DateChooser implements IDateChooserPresenter {
 		// normalize to year/month/date only
 		this.dateTime = new DateTime(dateTime.getYear(),
 				dateTime.getMonthOfYear(), dateTime.getDayOfMonth(), 0, 0, 0, 0);
-
-		dayCells = new ArrayList<IDayCell>(42);
-		for (int i = 0; i < 42; i++) {
-			DayCell dayCell = new DayCell();
-			dayCells.add(dayCell);
-		}
-		computeDayCellValues();
 	}
 
 	private void computeDayCellValues() {
