79055ebf6ba04edfa334a2dd32ac415713bccc94
Kai Toedter <kai.toedter@siemens.com>
2011-07-21 15:02:03 +0200

    EPL disclaimer

--- com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/IDateChooserPresenter.java
+++ com.toedter.jcalendar.core/src/com/toedter/jcalendar/core/IDateChooserPresenter.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Kai Tödter
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kai Tödter - initial API and implementation
+ *******************************************************************************/
+
 package com.toedter.jcalendar.core;
 
 import java.util.List;
